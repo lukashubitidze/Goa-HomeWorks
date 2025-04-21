666cbc167dc0efe3f46a80553b9fda2d644fb3c2
lukashubitidze <subitidzeluka@gmail.com>
2025-04-21 14:43:15 +0400

    commit

---
+++ day 10/homework/lesson10 #1.py
@@ -0,0 +1,3 @@
+print(True and not False and (False or True) and True or not False)
+#ეს კოდი გამოიტანს True-ს, რადგან ჯერ ნსრულდება ()- ში მოთავსებული,მერე სრულდება not, 
+# შემდეგ სრულდება and-ი და ბოლოს შესრულდება or-ი.
