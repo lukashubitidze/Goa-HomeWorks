e8fb58ea0164e35502a2213ed49a0e75a7c67926
lukashubitidze <subitidzeluka@gmail.com>
2025-04-29 14:37:10 +0400

    commit

---
+++ day 10/claswork/lesson10 #1.py
@@ -0,0 +1,12 @@
+print(True and False or False and False or True or True or False)
+
+# პირველო მოქმედება სრულდება true and false და გამოვა false, შემდეგ შესრულდება false and false
+#და გამოვა false, შემდეგ შესრულდება false or false და გამოვა false, შემდეგ შესრულდება false or true
+#და გამოვა true, შმდეგ შესრულდება true or true და გამოვა true, შემდეგ შესრულდება true or false და გამობა true.
+#პასუხია true
+
+
+#ლოგიკური ოპერაციების დროს პირველი სრყლდება and-ი
+
+
+# and ი გავს * ან /  -ს
