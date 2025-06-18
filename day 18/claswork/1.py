221a8ff42902a3b2e7ce9e2b00522606cc187c42
lukashubitidze <subitidzeluka@gmail.com>
2025-06-18 20:40:52 +0400

    'commit'

---
+++ day 18/claswork/1.py
@@ -0,0 +1,5 @@
+num = int(input('please input number: '))
+sum = 0
+for i in range(1,num):
+    sum = sum + i
+print(sum)
---
+++ day 18/claswork/2.py
@@ -0,0 +1,5 @@
+num = int(input('please input number: '))
+sum = 0
+for i in range(1,num,2):
+    sum = sum + i
+print(sum)
