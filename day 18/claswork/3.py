f7eb61a60dbb37e632122f133d235dc2758ca106
lukashubitidze <subitidzeluka@gmail.com>
2025-06-22 13:58:05 +0400

    'commit'

---
+++ day 18/claswork/3.py
@@ -0,0 +1,5 @@
+pasword = 'lukaluka'
+guess = input('please enter your pasword: ')
+while pasword != guess:
+    guess = input('incorect please try again: ')
+    print('congrats! you guesed corectly.' )
---
+++ day 18/homework/3.py
@@ -0,0 +1,5 @@
+num = 7
+guess = input('please enter your number 1-10: ')
+while num != guess:
+    guess = input('incorect number. please try again: ')
+    print('congrats! you guesed corectly.' )
