adb924b2c2b10f068612f814103ad90511db40bc
lukashubitidze <subitidzeluka@gmail.com>
2025-08-24 20:52:51 +0400

    'commit'

---
+++ day 34/claswork/set.py
@@ -0,0 +1,4 @@
+t1 = ("apple","mango","dragon_fruit","kivi","banana")
+print(t1[0])
+print(t1[2])
+print(t1[4])
---
+++ day 34/claswork/set1.py
@@ -0,0 +1,8 @@
+t =(1,2,3,4,5)
+print(t)
+
+t_list = list(t)
+t_list[2] = 100000
+
+new_t = (t_list)
+print(new_t)
